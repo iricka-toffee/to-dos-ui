13bba93aa00697183bc87e49df890971cd79155e
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-03-26 22:05:21 +0500

    feat: add generated TypeScript types

---
+++ src/api-types/index.ts
@@ -0,0 +1,27 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface ToDo {
+  id: number;
+  name: string;
+}
+
+export interface ToDosResponse {
+  toDos: ToDo[];
+}
+
+export interface NewToDoRequest {
+  name: string;
+}
+
+export interface NewToDoResponse {
+  newToDoId: number;
+}
