eff911ba026fd99f09849e9a5c35ab7a8bd3ff25
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-03-27 19:58:48 +0500

    test: ToDosState.cy.tsx
      GIVEN initial state with no ToDos
      WHEN initialize it with two ToDos
      SHOULD return them from toDos

--- src/pages/to-dos/sections/list/state/ToDosState.cy.tsx
+++ src/pages/to-dos/sections/list/state/ToDosState.cy.tsx
@@ -10,4 +10,27 @@ describe(`ToDosState`, () => {
 
     expect(toDosState.toDos).to.deep.eq([])
   })
+
+  it(`
+  GIVEN initial state with no ToDos
+  WHEN initialize it with two ToDos
+  SHOULD return them from toDos
+  `, () => {
+    const toDosState = new ToDosState()
+
+    const toDosForInitialization = [
+      {
+        name: `First Fizz`,
+      },
+      {
+        name: `Second Buzz`,
+      },
+    ]
+
+    toDosState.initialize({
+      toDos: toDosForInitialization,
+    })
+
+    expect(toDosState.toDos).to.deep.eq(toDosForInitialization)
+  })
 })
