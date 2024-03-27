0b5a0d6bff8ca1656ece0242fe66531725301f9f
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-03-27 21:02:34 +0500

    test: to-dos-smoke.cy.ts
      GIVEN ToDos page
      WHEN add a new ToDo
      SHOULD see it in the list

--- cypress/e2e/to-dos-smoke.cy.ts
+++ cypress/e2e/to-dos-smoke.cy.ts
@@ -10,4 +10,26 @@ describe(`ToDos Smoke`, () => {
       .get(`[data-cy="to-do"]`)
       .should(`have.length.greaterThan`, 0)
   })
+
+  it(`
+  GIVEN ToDos page
+  WHEN add a new ToDo
+  SHOULD see it in the list
+  `, () => {
+    cy.visit(`/to-dos`)
+
+    const newToDoName = `[E2E-SMOKE] ${new Date()}`
+
+    cy
+      .get(`[data-cy="new-to-do-name-input"]`)
+      .type(newToDoName)
+
+    cy
+      .get(`[data-cy="add-new-to-do-button"]`)
+      .click()
+
+    cy
+      .get(`[data-cy="to-dos"]`)
+      .contains(newToDoName)
+  })
 })
