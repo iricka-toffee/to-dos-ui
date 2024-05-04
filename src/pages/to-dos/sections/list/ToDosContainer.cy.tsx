6a06bfc12827554bc9cbc7bdd5e6b147ead0ce3c
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-05-04 14:59:54 +0500

    test: ToDosContainer.cy.tsx
      GIVEN two ToDos from network
      WHEN select both of them
      AND click Complete
      SHOULD make complete network call with the ids in the body

--- src/pages/to-dos/sections/list/ToDosContainer.cy.tsx
+++ src/pages/to-dos/sections/list/ToDosContainer.cy.tsx
@@ -1,3 +1,4 @@
+import { ToDosResponse } from "../../../../api-types"
 import { ToDosContainer } from "./ToDosContainer"
 import { ToDosState } from "./state/ToDosState"
 import { ToDosStateContext } from "./state/ToDosStateContext"
@@ -28,6 +29,62 @@ describe(`ToDosContainer`, () => {
     mountComponent()
 
     cy.contains(`Deux`)
+  })
+
+  it(`
+  GIVEN two ToDos from network
+  WHEN select both of them
+  AND click Complete
+  SHOULD make complete network call with the ids in the body
+  `, () => {
+    const toDosResponse: ToDosResponse = {
+      toDos: [
+        {
+          id: 1,
+          name: `Un`,
+        },
+        {
+          id: 2,
+          name: `Deux`,
+        },
+      ],
+    }
+
+    cy.intercept(
+      `GET`,
+      `*/to-dos`,
+      toDosResponse,
+    )
+
+    cy
+      .intercept(
+        `POST`,
+        `*/to-dos/complete`,
+        `true`, // because it needs this to be specified and to be string, even if it is not correct by the API contract
+      )
+      .as(`completeToDosNetworkCall`)
+
+    mountComponent()
+
+    cy
+      .contains(`Un`)
+      .click()
+
+    cy
+      .contains(`Deux`)
+      .click()
+
+    cy.get(`[data-cy=complete-selected-to-dos-button]`)
+      .click()
+
+    cy.get(`@completeToDosNetworkCall`)
+      .its(`request.body`)
+      .should(`deep.equal`, {
+        toDoIds: [
+          1,
+          2,
+        ],
+      })
   })
 })
 
