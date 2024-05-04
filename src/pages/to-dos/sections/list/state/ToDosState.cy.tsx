673b3dd50830d7aff34b5198bdeb18b246a54aa2
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-05-04 13:35:59 +0500

    test: ToDosState.cy.tsx
      GIVEN three ToDos
      WHEN select them one by one
      SHOULD end up in list of selected ids

--- src/pages/to-dos/sections/list/state/ToDosState.cy.tsx
+++ src/pages/to-dos/sections/list/state/ToDosState.cy.tsx
@@ -34,4 +34,52 @@ describe(`ToDosState`, () => {
 
     expect(toDosState.toDos).to.deep.eq(toDosForInitialization)
   })
+
+  it(`
+  GIVEN three ToDos
+  WHEN select them one by one
+  SHOULD end up in list of selected ids
+  `, () => {
+    const toDosState = new ToDosState()
+
+    const toDosForInitialization = [
+      {
+        id: 1,
+      },
+      {
+        id: 2,
+      },
+      {
+        id: 3,
+      },
+    ] as ToDo[]
+
+    toDosState.initialize({
+      toDos: toDosForInitialization,
+    })
+
+    expect(toDosState.selectedToDoIds).to.deep.eq([])
+
+    toDosState.toggleToDoSelection({
+      toDoId: 2,
+    })
+
+    expect(toDosState.selectedToDoIds).to.deep.eq([
+      2,
+    ])
+
+    toDosState.toggleToDoSelection({
+      toDoId: 1,
+    })
+
+    toDosState.toggleToDoSelection({
+      toDoId: 3,
+    })
+
+    expect(toDosState.selectedToDoIds).to.deep.eq([
+      2,
+      1,
+      3,
+    ])
+  })
 })
