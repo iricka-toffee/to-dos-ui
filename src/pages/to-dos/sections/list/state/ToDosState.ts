eb4114090e8a90d3d01d3e8bcb38146ff5e3e0ee
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-05-04 13:38:10 +0500

    feat: implement the possibility to select ToDos and read the selected ones ids

--- src/pages/to-dos/sections/list/state/ToDosState.ts
+++ src/pages/to-dos/sections/list/state/ToDosState.ts
@@ -4,6 +4,7 @@ import { ToDo } from '../../../../../api-types'
 export class ToDosState {
   private _toDos: ToDo[] = []
   private _needToReloadToDos: boolean = false
+  private _selectedToDoIds: number[] = []
 
   constructor() {
     makeAutoObservable(this)
@@ -21,8 +22,20 @@ export class ToDosState {
     return this._toDos
   }
 
+  get selectedToDoIds() {
+    return this._selectedToDoIds
+  }
+
   get needToReloadToDos() {
     return this._needToReloadToDos
+  }
+
+  toggleToDoSelection({
+    toDoId,
+  }: {
+    toDoId: number,
+  }) {
+    this._selectedToDoIds.push(toDoId)
   }
 
   triggerToDosReload() {
