9e7a57942419e76907a38ac79d462ffdaf8e8c9c
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-05-04 15:31:36 +0500

    test: CompleteToDosButton.cy.tsx
      GIVEN no ToDos
      WHEN render the component
      SHOULD see the disabled button

---
+++ src/pages/to-dos/sections/list/components/complete-to-dos-button/CompleteToDosButton.cy.tsx
@@ -0,0 +1,47 @@
+import { ToDo } from "../../../../../../api-types"
+import { ToDosState } from "../../state/ToDosState"
+import { ToDosStateContext } from "../../state/ToDosStateContext"
+
+describe(`CompleteToDosButton`, () => {
+  describe(`Initialization`, initializationTests)
+})
+
+function initializationTests() {
+  it(`
+  GIVEN no ToDos
+  WHEN render the component
+  SHOULD see the disabled button
+  `, () => {
+    mountComponent({
+      toDosForInitialization: [],
+    })
+
+    cy
+      .get(`[data-cy=complete-selected-to-dos-button]`)
+      .should(`be.disabled`)
+  })
+}
+
+function mountComponent({
+  toDosForInitialization,
+}: {
+  toDosForInitialization: unknown[],
+}) {
+  const toDosState = new ToDosState()
+  
+  toDosState.initialize({
+    toDos: toDosForInitialization as ToDo[],
+  })
+
+  cy
+    .wrap(toDosState)
+    .as(`toDosState`)
+
+  cy.mount(
+    <ToDosStateContext.Provider value={toDosState}>
+      <CompleteToDosButton
+        onClick={() => {}}
+      />
+    </ToDosStateContext.Provider>,
+  )
+}
