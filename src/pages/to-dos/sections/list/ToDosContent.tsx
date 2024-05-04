78285c32c7842a73c280a0c600fb26c78973e483
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2024-05-04 14:50:42 +0500

    feat: implement disabled Complete button when there is nothing selected

--- src/pages/to-dos/sections/list/ToDosContent.tsx
+++ src/pages/to-dos/sections/list/ToDosContent.tsx
@@ -6,36 +6,45 @@ export const ToDosContent = observer(() => {
   const toDosState = useContext(ToDosStateContext)
 
   return (
-    <ul
-      data-cy="to-dos"
-    >
-      {
-        toDosState
-          .toDos
-          .map(({
-            id,
-            name,
-          }) => (
-            <li
-              key={`to-do-${id}`}
-              data-cy="to-do"
-            >
-              <input
-                id={`to-do-${id}-checkbox`}
-                type="checkbox"
-                onChange={() => toDosState.toggleToDoSelection({
-                  toDoId: id, 
-                })}
-                checked={toDosState.selectedToDoIds.includes(id)}
-              />
-              <label 
-                htmlFor={`to-do-${id}-checkbox`}
+    <>
+      <button
+        type="button"
+        data-cy="complete-selected-to-dos-button"
+        disabled={toDosState.selectedToDoIds.length === 0}
+      >
+        Complete
+      </button>
+      <ul
+        data-cy="to-dos"
+      >
+        {
+          toDosState
+            .toDos
+            .map(({
+              id,
+              name,
+            }) => (
+              <li
+                key={`to-do-${id}`}
+                data-cy="to-do"
               >
-                {name}
-              </label>
-            </li>
-          ))
-      }
-    </ul>
+                <input
+                  id={`to-do-${id}-checkbox`}
+                  type="checkbox"
+                  onChange={() => toDosState.toggleToDoSelection({
+                    toDoId: id, 
+                  })}
+                  checked={toDosState.selectedToDoIds.includes(id)}
+                />
+                <label 
+                  htmlFor={`to-do-${id}-checkbox`}
+                >
+                  {name}
+                </label>
+              </li>
+            ))
+        }
+      </ul>
+    </>
   )
 })
