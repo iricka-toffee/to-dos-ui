b64b63372c7337d9b9d338210a77b2533fa05aa8
fpandyz <5389368+fpandyz@users.noreply.github.com>
2025-03-25 11:31:03 +0500

    fix: align local-env port to be 30080 (#8)
    Co-authored-by: Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>

--- cypress.config.local-env.ts
+++ cypress.config.local-env.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "cypress"
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   e2e: {
-    baseUrl: `http://localhost:40080`,
+    baseUrl: `http://localhost:30080`,
   },
   env: {
-    API_URL: `http://localhost:40080/api/to-dos-api`,
+    API_URL: `http://localhost:30080/api/to-dos-api`,
   }
 })
