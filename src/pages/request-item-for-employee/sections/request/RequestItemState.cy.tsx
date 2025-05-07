bd9afb96cfe0ae4f4aad61c3e430243af6f864e8
Yellowgree <zmo-1002@mail.ru>
2025-05-07 17:15:27 +0500

    test: requestItemState
    GIVEN initial state of the form
    WHEN ask for description
    SHOULD return an empty string

---
+++ src/pages/request-item-for-employee/sections/request/RequestItemState.cy.tsx
@@ -0,0 +1,9 @@
+it(`
+  GIVEN initial state of the form
+  WHEN ask for description
+  SHOULD return an empty string
+  `, () => {
+  const requestItemState = new RequestItemState()
+
+  expect(requestItemState.description).to.eq(``)
+})
