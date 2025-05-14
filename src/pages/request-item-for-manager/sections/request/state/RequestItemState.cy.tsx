7ddb81930598c4325c4208422079b611b2ec12b9
irina <>
2025-05-14 16:31:13 +0500

    Revert "test: RequestItemState.cy.tsx"
    This reverts commit 1d8f15a257d0008c0f75998054f4498b02a61690.

--- src/pages/request-item-for-manager/sections/request/state/RequestItemState.cy.tsx
+++
@@ -1,9 +0,0 @@
-it(`
-  GIVEN initial state of the form
-  WHEN ask for max price
-  SHOULD return null
-  `, () => {
-  const requestItemState = new RequestItemState()
-
-  expect(requestItemState.maxPrice).to.eq(null)
-})
