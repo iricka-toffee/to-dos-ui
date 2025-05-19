7333971f6b5c98dd04f02984931f3cfd09c25680
ARTYsas <128177662+ARTYsas@users.noreply.github.com>
2025-05-19 23:22:48 +0500

    feat(request-form): add label for item type selection
    Renders a <label> with htmlFor="type" and text "Тип".

--- src/pages/request-item-for-manager/sections/request/state/RequestForm.cy.tsx
+++ src/pages/request-item-for-manager/sections/request/state/RequestForm.cy.tsx
@@ -1,6 +1,8 @@
-// RequestForm.cy.tsx
 import { RequestForm } from './RequestForm'
 
+//test 1
+
+/*
 it(`
   GIVEN RequestForm
   WHEN it is mounted
@@ -9,3 +11,18 @@ it(`
   cy.mount(<RequestForm />)
   cy.get('form').should('exist')
 })
+*/
+
+//test 2
+
+
+it(`
+  GIVEN RequestForm
+  WHEN it is mounted
+  SHOULD render label with text "Тип"
+`, () => {
+  cy.mount(<RequestForm />)
+  cy.get('label[for="type"]').should('exist').and('contain.text', 'Тип')
+})
+
+
--- src/pages/request-item-for-manager/sections/request/state/RequestForm.tsx
+++ src/pages/request-item-for-manager/sections/request/state/RequestForm.tsx
@@ -1,7 +1,8 @@
-// src/components/RequestForm.tsx
+
 export function RequestForm() {
   return (
     <form>
+      <label htmlFor="type">Тип</label>
     </form>
   );
 }
