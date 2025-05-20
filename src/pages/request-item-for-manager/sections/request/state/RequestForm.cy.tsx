3c3ca2ee374db991c85e987d7d5e0351b37e517d
ARTYsas <128177662+ARTYsas@users.noreply.github.com>
2025-05-20 18:35:15 +0500

    Revert "feat(request-form): add options for type select"
    This reverts commit 518b30d4d9e8dbb5da9fbce4955a495b3eaabc33.
    
    # Conflicts:
    #	src/pages/request-item-for-manager/sections/request/state/RequestForm.cy.tsx
    #	src/pages/request-item-for-manager/sections/request/state/RequestForm.tsx

--- src/pages/request-item-for-manager/sections/request/state/RequestForm.cy.tsx
+++ src/pages/request-item-for-manager/sections/request/state/RequestForm.cy.tsx
@@ -28,7 +28,7 @@ it(`
 
 //test 3
 
-/*
+
 it(`
   GIVEN RequestForm
   WHEN it is mounted
@@ -37,42 +37,5 @@ it(`
   cy.mount(<RequestForm />)
   cy.get('select#type').should('exist').and('be.visible')
 })
-*/
-
-//test 4
-
-/*
-it(`
-  GIVEN RequestForm
-  WHEN it is mounted
-  SHOULD render "Monitor" and "Laptop" as select options
-`, () => {
-  cy.mount(<RequestForm />)
-
-  cy.get('select#type option')
-    .should('have.length.at.least', 2)
-    .then((options) => {
-      const values = [...options].map((o) => o.textContent)
-      expect(values).to.include.members(['Monitor', 'Laptop'])
-    })
-})
-*/
-
-//test 5
 
 
-it(`
-  GIVEN RequestForm
-  WHEN it is mounted
-  SHOULD render number input with label "Максимальная цена"
-`, () => {
-  cy.mount(<RequestForm />)
-
-  cy.get('label[for="maxPrice"]')
-    .should('exist')
-    .and('contain.text', 'Максимальная цена')
-
-  cy.get('input#maxPrice')
-    .should('exist')
-    .and('have.attr', 'type', 'number')
-})
--- src/pages/request-item-for-manager/sections/request/state/RequestForm.tsx
+++ src/pages/request-item-for-manager/sections/request/state/RequestForm.tsx
@@ -3,13 +3,7 @@ export function RequestForm() {
   return (
     <form>
       <label htmlFor="type">Тип</label>
-      <select id="type">
-        <option>Monitor</option>
-        <option>Laptop</option>
-      </select>
-
-      <label htmlFor="maxPrice">Максимальная цена</label>
-      <input type="number" id="maxPrice" />
+      <select id="type"></select>
     </form>
   )
 }
