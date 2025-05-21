3ba0ce4d37ea17f46a2a8e5c81285085a54ec40f
irina <>
2025-05-21 13:16:50 +0500

    test: add test for CreateItemContent dropdown with available types
    GIVEN initial CreateItemState
    WHEN render the component
    SHOULD show a dropdown with available types

---
+++ src/pages/create-item/sections/create/CreateItemContent.cy.tsx
@@ -0,0 +1,48 @@
+import { CreateItemContent } from "./CreateItemContent"
+import { CreateItemState } from "./state/CreateItemState"
+import { CreateItemStateContext } from "./state/CreateItemStateContext"
+
+describe(`CreateItemContent`, () => {
+  describe(`Initialization`, initializationTests)
+})
+
+function initializationTests() {
+  it(`
+  GIVEN initial CreateItemState
+  WHEN render the component
+  SHOULD show a dropdown with available types
+  `, () => {
+    mountComponent()
+
+    cy.get(`select`)
+      .should(`exist`)
+
+    cy.get(`option`)
+      .then((options) => {
+        const optionValues = [
+          ...options,
+        ].map((o) => o.text)
+        expect(optionValues).to.include.members([
+          `Laptop`,
+          `DesktopComputer`,
+          `Chair`,
+          `Monitor`,
+          `Keyboard`,
+          `Mouse`,
+        ])
+      })
+  })
+}
+
+function mountComponent() {
+  const state = new CreateItemState()
+
+  cy.wrap(state)
+    .as(`createItemState`)
+
+  cy.mount(
+    <CreateItemStateContext.Provider value={state}>
+      <CreateItemContent onSubmitClick={() => Promise.resolve()} />
+    </CreateItemStateContext.Provider>,
+  )
+}
