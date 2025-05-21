bd69d6270c0ed15558577703c1bb0d9de22bf6c2
irina <>
2025-05-21 16:27:02 +0500

    fix: cast null as CreateItemState

--- src/pages/create-item/sections/create/state/CreateItemStateContext.ts
+++ src/pages/create-item/sections/create/state/CreateItemStateContext.ts
@@ -1,4 +1,4 @@
 import { createContext } from "react"
 import { CreateItemState } from "./CreateItemState"
 
-export const CreateItemStateContext = createContext<CreateItemState | null>(null)
+export const CreateItemStateContext = createContext<CreateItemState>(null as unknown as CreateItemState)
