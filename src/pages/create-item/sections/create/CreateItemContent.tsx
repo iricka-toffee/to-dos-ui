f0fe9bb9d67abb6c22f5301e4b706dcbe2fbc1ad
irina <>
2025-05-21 13:17:48 +0500

    feat: add dropdown component with available types using MobX

---
+++ src/pages/create-item/sections/create/CreateItemContent.tsx
@@ -0,0 +1,42 @@
+import { observer } from "mobx-react-lite"
+import { useContext } from "react"
+import { CreateItemStateContext } from "./state/CreateItemStateContext"
+
+type CreateItemContent = {
+  onSubmitClick: () => Promise<void>,
+}
+
+export const CreateItemContent = observer(({
+  onSubmitClick,
+}: CreateItemContent) => {
+  const state = useContext(CreateItemStateContext)
+
+  if (!state) {
+    return <div>Loading...</div>
+  }
+
+  const handleChange = (value: string) => {
+    state.setType(value)
+    onSubmitClick()
+  }
+
+  return (
+    <div>
+      <label htmlFor="type">Type:</label>
+      <select
+        id="type"
+        value={state.type}
+        onChange={(e) => handleChange(e.target.value)}
+      >
+        <option value=""
+          disabled>Select a type</option>
+        {state.availableTypes.map((type) => (
+          <option key={type}
+            value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+})
